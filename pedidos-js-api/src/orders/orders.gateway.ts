e6118e566dff5694a8afe4b3325c0f5b637c76ca
Thiago Elias <thiagoelias99@gmail.com>
2026-01-08 16:19:11 -0300

    autorização basica

--- pedidos-js-api/src/orders/orders.gateway.ts
+++ pedidos-js-api/src/orders/orders.gateway.ts
@@ -1,5 +1,5 @@
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets'
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 import { OrderView } from './view/order.view'
 
 @WebSocketGateway({
@@ -11,15 +11,47 @@ export class OrdersGateway {
   @WebSocketServer()
   server: Server
 
+  handleConnection(client: Socket) {
+    /**
+     * Exemplo simples:
+     * frontend envia role no handshake
+     * depois você troca isso por JWT sem mudar a arquitetura
+     */
+    console.log('Novo cliente conectado:', client.id)
+    console.log('Handshake auth:', client)
+
+    const authorization = client.handshake.headers.authorization
+    client.data.authorization = authorization
+
+    // aqui pode fazer várias lógicas de autorização e incluir o cliente em salas específicas
+    // se for admin, entra na sala dos admins
+    if (authorization === 'admin') {
+      client.join('role:admin')
+      console.log(`Cliente conectado como admin`)
+    } else {
+      console.log(`Cliente conectado sem role`)
+    }
+
+    // enviar mensagem de boas-vindas para o cliente conectado
+    client.emit('message', 'Conexão estabelecida com sucesso!')
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Cliente desconectado:', client.id)
+  }
+
   emitOrderCreated(order: OrderView) {
+    // Emite para todos os clientes conectados
     this.server.emit('orderCreated', order)
   }
 
   emitOrderUpdated(order: OrderView) {
+    // Emite para todos os clientes conectados
     this.server.emit('orderUpdated', order)
   }
 
   emitOrderDeleted(order: OrderView) {
-    this.server.emit('orderDeleted', order)
+    // Emite apenas para clientes na sala de admins
+    this.server.to('role:admin').emit('orderDeleted', order)
   }
 }
